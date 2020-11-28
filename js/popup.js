4016ca557594c8df56a1694e3ffb7444a4d765c5
Anand Kumar <kumarabhishek31853@gmail.com>
2020-11-28 21:58:38 +0530

    completed wishes page

---
+++ js/popup.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById("popup__box");
+
+displayPopup = function () {
+    window.setTimeout(function () {
+        modal.style.display = "block";
+    }, 100);
+
+};
+
+closePopup = function () {
+    modal.style.display = "none";
+};
+
+var btn = $(".popup-btn").click(displayPopup);
+var closeBox = $(".close__box").click(closePopup);
+modal.style.display = "block";
+
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
